74016bccea1f57f8418193dd816277289e43f1a8
Aya <loligafeo@gmail.com>
2022-02-19 00:48:31 +0800

    项目模块拆分、设计

---
+++ miko-base/src/main/java/com/macoloco/mikobase/MikoBaseApplication.java
@@ -0,0 +1,13 @@
+package com.macoloco.mikobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikoBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikoBaseApplication.class, args);
+    }
+
+}
---
+++ miko-base/src/test/java/com/macoloco/mikobase/MikoBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macoloco.mikobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikoBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
